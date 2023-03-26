d2e38fc552eb2a2381159e6559695e5b4279af38
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-03-26 12:11:45 +0100

    Create the basic route and service for the user module

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  createUser() {
+    return 'This action adds a new user';
+  }
+}
