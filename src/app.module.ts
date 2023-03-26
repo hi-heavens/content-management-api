93d119bbad20bafce14255f49ddc73f39fd44c84
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-03-26 12:00:48 +0100

    created the user, post and category modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PostModule, CategoryModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
