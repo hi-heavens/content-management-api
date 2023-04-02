8cdbbc6ab0778b8038cd91aac4246c772e73cb19
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-04-02 12:01:39 +0100

    changed the port to use env port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3003;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3003);
+  await app.listen(PORT);
 }
 bootstrap();
